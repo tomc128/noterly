a967699f1b02bd763d01a0a03a8b2c9aee44cf03
Tom Chapman <tomchapman128@gmail.com>
2023-01-15 21:21:38 +0000

    Add new font and prepare for material 3 with dynamic colour

--- noti_buddy/lib/main.dart
+++ noti_buddy/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:noti_buddy/managers/isolate_manager.dart';
 import 'package:noti_buddy/pages/main_page.dart';
 
@@ -15,12 +17,52 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Noti Buddy',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const MainPage(),
-    );
+    return DynamicColorBuilder(builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+      ColorScheme lightColorScheme;
+      ColorScheme darkColorScheme;
+
+      if (lightDynamic != null && darkDynamic != null) {
+        // On Android S+ devices, use the provided dynamic color scheme.
+        // (Recommended) Harmonize the dynamic color scheme' built-in semantic colors.
+        lightColorScheme = lightDynamic.harmonized();
+        // (Optional) Customize the scheme as desired. For example, one might
+        // want to use a brand color to override the dynamic [ColorScheme.secondary].
+        // lightColorScheme = lightColorScheme.copyWith(secondary: _brandBlue);
+
+        // Repeat for the dark color scheme.
+        darkColorScheme = darkDynamic.harmonized();
+      } else {
+        print('No dynamic color scheme, using fallback.');
+        // Otherwise, use fallback schemes.
+        lightColorScheme = ColorScheme.fromSeed(
+          seedColor: Colors.red,
+        );
+        darkColorScheme = ColorScheme.fromSeed(
+          seedColor: Colors.red,
+          brightness: Brightness.dark,
+        );
+      }
+
+      return MaterialApp(
+        title: 'Noti Buddy',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: lightColorScheme,
+          fontFamily: GoogleFonts.dmSans().fontFamily,
+
+          // scaffoldBackgroundColor: lightColorScheme.background,
+        ),
+        darkTheme: ThemeData(
+          useMaterial3: true,
+          colorScheme: darkColorScheme,
+          fontFamily: GoogleFonts.dmSans().fontFamily,
+          // scaffoldBackgroundColor: darkColorScheme.surface.harmonizeWith(darkColorScheme.background),
+        ),
+        themeMode: ThemeMode.system,
+        home: const MainPage(),
+
+        // Theme.of(context).textTheme.
+      );
+    });
   }
 }
--- noti_buddy/macos/Flutter/GeneratedPluginRegistrant.swift
+++ noti_buddy/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import dynamic_color
 import flutter_local_notifications
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
