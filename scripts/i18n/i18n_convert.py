c58f5a4df7b6c47d00a9b27e594694dbed7a3e16
Tom Chapman <tomchapman128@gmail.com>
2023-02-05 18:07:30 +0000

    Modify script to use nested objects

--- scripts/i18n/i18n_convert.py
+++ scripts/i18n/i18n_convert.py
@@ -1,5 +1,17 @@
 import json
 
+
+def create_nested_dict(key_parts, value, d):
+    if type(d) != dict:
+        d = {}
+    if len(key_parts) == 1:
+        d[key_parts[0]] = value
+        return d
+    if key_parts[0] not in d:
+        d[key_parts[0]] = {}
+    d[key_parts[0]] = create_nested_dict(key_parts[1:], value, d[key_parts[0]])
+    return d
+
 with open('i18n.json', 'r') as f:
     i18n = json.load(f)
 
@@ -9,7 +21,8 @@ for key, value in i18n['text'].items():
     for lang, text in value.items():
         if lang not in lang_data:
             lang_data[lang] = {}
-        lang_data[lang][key] = text
+        key_parts = key.split(".")
+        lang_data[lang] = create_nested_dict(key_parts, text, lang_data[lang])
 
 for lang, data in lang_data.items():
     with open(f'out/{lang}.json', 'w') as f:
