e3d69ec2c59d2141f3ec17617f24e95c34247b99
Tom Chapman <tomchapman128@gmail.com>
2023-03-02 15:02:20 +0000

    Add additional build info to build task

--- noterly/lib/build_info.dart
+++ noterly/lib/build_info.dart
@@ -1,3 +1,7 @@
 class BuildInfo {
   static const String appVersion = 'UNSET_VERSION';
+  static const ReleaseType releaseType = ReleaseType.beta;
+  static const String branch = 'UNSET_BRANCH';
 }
+
+enum ReleaseType { stable, beta }
