6790328b77d9b4a580e87529cca42e467fc03b10
Tom Chapman <tomchapman128@gmail.com>
2023-08-26 22:24:36 +0100

    Convert datetime extensions to new translation system

--- noterly/lib/extensions/date_time_extensions.dart
+++ noterly/lib/extensions/date_time_extensions.dart
@@ -1,6 +1,7 @@
-import 'package:flutter_translate/flutter_translate.dart';
 import 'package:intl/intl.dart';
 import 'package:jiffy/jiffy.dart';
+import 'package:noterly/l10n/localisations_util.dart';
+import 'package:noterly/main.dart';
 
 extension DateTimeExtensions on DateTime {
   bool isToday() => DateTime.now().year == year && DateTime.now().month == month && DateTime.now().day == day;
@@ -15,19 +16,20 @@ extension DateTimeExtensions on DateTime {
 
   String toRelativeDateTimeString({bool alwaysShowDay = false}) {
     final now = DateTime.now();
+    final context = MyApp.navigatorKey.currentContext!;
 
     bool isToday() => now.year == year && now.month == month && now.day == day;
     bool isYesterday() => now.year == year && now.month == month && now.day - day == 1;
     bool isTomorrow() => now.year == year && now.month == month && now.day - day == -1;
     bool isBeforeNextWeek() => isBefore(now.add(const Duration(days: 7)));
 
-    if (isToday()) return alwaysShowDay ? translate('time.today_and_time', args: {'time': toTimeOnlyString()}) : Jiffy.parseFromDateTime(this).fromNow();
+    if (isToday()) return alwaysShowDay ? Strings.of(context).time_todayAndTime(toTimeOnlyString()) : Jiffy.parseFromDateTime(this).fromNow();
 
-    if (isYesterday()) return translate('time.yesterday_and_time', args: {'time': toTimeOnlyString()});
+    if (isYesterday()) return Strings.of(context).time_yesterdayAndTime(toTimeOnlyString());
 
-    if (isTomorrow()) return translate('time.tomorrow_and_time', args: {'time': toTimeOnlyString()});
+    if (isTomorrow()) return Strings.of(context).time_tomorrowAndTime(toTimeOnlyString());
 
-    if (isBeforeNextWeek()) return translate('time.date_and_time', args: {'date': DateFormat.EEEE().format(this), 'time': toTimeOnlyString()});
+    if (isBeforeNextWeek()) return Strings.of(context).time_dateAndTime(DateFormat.EEEE().format(this), toTimeOnlyString());
 
     if (year == now.year) return toDateTimeString();
 
@@ -36,33 +38,28 @@ extension DateTimeExtensions on DateTime {
 
   String toSnoozedUntilDateTimeString() {
     final now = DateTime.now();
+    final context = MyApp.navigatorKey.currentContext!;
 
     bool isToday() => now.year == year && now.month == month && now.day == day;
     bool isYesterday() => now.year == year && now.month == month && now.day - day == 1;
     bool isTomorrow() => now.year == year && now.month == month && now.day - day == -1;
     bool isBeforeNextWeek() => isBefore(now.add(const Duration(days: 7)));
 
-    if (isToday()) return translate('time.snooze.until', args: {'date_time': toTimeOnlyString()});
+    if (isToday()) return Strings.of(context).time_snooze_until(toTimeOnlyString());
 
     if (isYesterday()) {
-      return translate('time.snooze.until', args: {
-        'date_time': translate('time.yesterday_and_time', args: {'time': toTimeOnlyString()})
-      });
+      return Strings.of(context).time_snooze_until(Strings.of(context).time_yesterdayAndTime(toTimeOnlyString()));
     }
 
     if (isTomorrow()) {
-      return translate('time.snooze.until', args: {
-        'date_time': translate('time.tomorrow_and_time', args: {'time': toTimeOnlyString()})
-      });
+      return Strings.of(context).time_snooze_until(Strings.of(context).time_tomorrowAndTime(toTimeOnlyString()));
     }
 
     if (isBeforeNextWeek()) {
-      return translate('time.snooze.until', args: {
-        'date_time': translate('time.date_and_time', args: {'date': DateFormat.EEEE().format(this), 'time': toTimeOnlyString()})
-      });
+      return Strings.of(context).time_snooze_until(Strings.of(context).time_dateAndTime(DateFormat.EEEE().format(this), toTimeOnlyString()));
     }
 
-    if (year == now.year) return translate('time.snooze.until', args: {'date_time': toDateTimeString()});
+    if (year == now.year) return Strings.of(context).time_snooze_until(toDateTimeString());
 
     return toDateTimeWithYearString();
   }
