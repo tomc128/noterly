84c6ee5ae8986a94c16490106173882eda8f7701
Tom Chapman <tomchapman128@gmail.com>
2023-08-26 22:27:52 +0100

    Convert duration extensions to new translation system

--- noterly/lib/extensions/duration_extensions.dart
+++ noterly/lib/extensions/duration_extensions.dart
@@ -1,4 +1,5 @@
-import 'package:flutter_translate/flutter_translate.dart';
+import 'package:noterly/l10n/localisations_util.dart';
+import 'package:noterly/main.dart';
 
 extension DurationExtensions on Duration {
   String toRelativeDurationString() {
@@ -6,21 +7,11 @@ extension DurationExtensions on Duration {
     final hours = inHours % 24;
     final minutes = inMinutes % 60;
 
-    final daysString = days == 0
-        ? ''
-        : days == 1
-            ? '1 ${translate('time.day')}'
-            : translate('time.days.value', args: {'value': days});
-    final hoursString = hours == 0
-        ? ''
-        : hours == 1
-            ? '1 ${translate('time.hour')}'
-            : translate('time.hours.value', args: {'value': hours});
-    final minutesString = minutes == 0
-        ? ''
-        : minutes == 1
-            ? '1 ${translate('time.minute')}'
-            : translate('time.minutes.value', args: {'value': minutes});
+    final context = MyApp.navigatorKey.currentContext!;
+
+    final daysString = days == 0 ? '' : Strings.of(context).time_days_value(days);
+    final hoursString = hours == 0 ? '' : Strings.of(context).time_hours_value(hours);
+    final minutesString = minutes == 0 ? '' : Strings.of(context).time_minutes_value(minutes);
 
     final combined = '$daysString, $hoursString, $minutesString'.trim();
     return combined.replaceAll(RegExp(r', ,'), ',').replaceAll(RegExp(r',$'), '').replaceAll(RegExp(r'^,\s*'), ''); // Remove trailing, leading or double commas
