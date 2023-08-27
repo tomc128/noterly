020f4ff9633b8b2b83ce59cb7d645fa5d847047e
Tom Chapman <tomchapman128@gmail.com>
2023-08-27 11:54:52 +0100

    Update backward format copy script to hopefully handle placeholder renames

--- scripts/l10n/backward_format_copy.py
+++ scripts/l10n/backward_format_copy.py
@@ -6,8 +6,10 @@
 
 import json
 import os
+import re
 
 SOURCE_ARB_FILE = '../noterly/lib/l10n/app_en.arb'
+GENERATED_ARB_FILE = 'l10n/out/initial_format_conversion/en.arb'
 
 LANGUAGE_JSON_FOLDER = '../noterly/assets/i18n'
 
@@ -55,24 +57,54 @@ def load_language_file(language_file):
 def escape_string(content):
     return content.replace("'", "''")
 
+def compare_and_fix_arb_strings(source_string, generated_string, new_string):
+    # this is if we have changed a placeholder name, i.e.
+    # generated_string: 'After {duration}'
+    # source_string: 'After {durationString}'
+    # so new_string will be 'After {duration}', we need to replace {duration} with {durationString}
+
+    if '{' not in source_string:
+        return new_string
+    
+    pattern = r'{\w+}'
+
+    source_placeholders = re.findall(pattern, source_string)
+    generated_placeholders = re.findall(pattern, generated_string)
+
+    if len(source_placeholders) != len(generated_placeholders):
+        print(f'Placeholder count mismatch. Source: {source_string}, Generated: {generated_string}, New: {new_string}')
+        return new_string
+    
+    for i, source_placeholder in enumerate(source_placeholders):
+        generated_placeholder = generated_placeholders[i]
+
+        if source_placeholder != generated_placeholder:
+            new_string = new_string.replace(generated_placeholder, source_placeholder)
+    
+    return new_string
+    
+
 def convert_format(language_code, language_content):
-    new_json = {}
+    new_content = {}
 
     for arb_key in arb_content.keys():
         if arb_key.startswith('@'):
             # special key - copy from source
-            new_json[arb_key] = arb_content[arb_key]
+            new_content[arb_key] = arb_content[arb_key]
             continue
 
         json_key = arb_to_json_key(arb_key)
         
         if json_key in language_content:
             string = escape_string(language_content[json_key])
-            new_json[arb_key] = string
+            string = compare_and_fix_arb_strings(arb_content[arb_key], generated_arb_content[arb_key], string)
+            new_content[arb_key] = string
         else:
-            new_json[arb_key] = '**MISSING**'
+            new_content[arb_key] = '**MISSING**'
 
-    return new_json
+    new_content['@@locale'] = language_code
+
+    return new_content
 
 
 language_json_contents = {}
@@ -81,6 +113,9 @@ new_arb_contents = {}
 
 with open(SOURCE_ARB_FILE, 'r', encoding='utf-8') as f:
     arb_content = json.load(f)
+
+with open(GENERATED_ARB_FILE, 'r', encoding='utf-8') as f:
+    generated_arb_content = json.load(f)
 
 
 for language_file in os.listdir(LANGUAGE_JSON_FOLDER):
